e05e8d1128f4ba820f147a73126a4ef82db2ba41
livvy <livvyguo@126.com>
2015-01-16 19:05:46 +0800

    identicon

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -2,3 +2,5 @@
 # -*-coding: utf-8 -*-
 # 包含了所有的依赖类 逻辑处理 等
 __author__ = 'livvy'
+
+__all__ = ["thirdparty","utils"]
---
+++ src/lib/utils/image.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+__author__ = 'livvy'
+
+import random
+import Image, ImageDraw, ImageFont, ImageFilter
+
+
